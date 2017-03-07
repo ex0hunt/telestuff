cdb4fef7d7fe5eed5d2caba5af4ebd80e98d8fa1
Dmitry K <dmitry@exohunt.me>
2017-03-07 19:29:59 +0300

    Check messages in new style - by handlers

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telegram
+from telegram.ext import Updater, MessageHandler, Filters
 import configparser
 
 config = configparser.ConfigParser()
@@ -10,39 +10,25 @@ def get_banlist():
     return [int(i) for i in banlist.split(',') if i]
 
 
-class TeleBot:
-    def __init__(self):
-        self.token = str(config.get('Main', 'token'))
-        self.bot = telegram.Bot(token=self.token)
+def ban_fwd(bot, update):
+    msg = update.message
+    chat_id = msg.chat.id
 
-    def main_func(self):
-        for m in self.bot.getUpdates(timeout=10):
-            if not m.message:
-                continue
-
-            is_forwarded = m.message.forward_from
-            forward_date = m.message.forward_date
-            chat_id = m.message.chat_id
-
-            if is_forwarded or forward_date:
-                if not m.message.forward_from_chat:
-                    continue
-                if m.message.forward_from_chat.id in get_banlist():
-                    try:
-                        self.bot.kickChatMember(chat_id, m.message.from_user.id)
-                        self.bot.sendMessage(chat_id=chat_id,
-                                             text='%s отправлен в Вальхаллу [🔥SPAM]' % m.message.from_user.name)
-                    except Exception as e:
-                        print(e)
-                        self.bot.sendMessage(chat_id=chat_id,
-                                             text='%s спамит, но забанить не могу 😭' % m.message.from_user.name)
-
-            self.confirm_updates(m.update_id)
-
-    def confirm_updates(self, update_id):
-        self.bot.getUpdates(offset=update_id+1, timeout=20)
+    if msg.forward_from_chat.id in get_banlist():
+        try:
+            bot.kickChatMember(chat_id, msg.from_user.id)
+            bot.sendMessage(chat_id=chat_id,
+                            text='%s отправлен в Вальхаллу [🔥SPAM]' % msg.from_user.name)
+        except Exception as e:
+            print(e)
+            bot.sendMessage(chat_id=chat_id,
+                            text='%s спамит, но забанить не могу 😭' % msg.from_user.name)
 
 
 if __name__ == '__main__':
-    while True:
-        TeleBot().main_func()
+    updater = Updater(str(config.get('Main', 'token')))
+
+    updater.dispatcher.add_handler(MessageHandler(Filters.forwarded, ban_fwd))
+
+    updater.start_polling()
+    updater.idle()
