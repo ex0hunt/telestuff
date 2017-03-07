5113a80f83ab6fa1cfb67e9ac97c3e2865e28000
Dmitry K <dmitry@exohunt.me>
2017-03-07 13:42:53 +0300

    Skip empty messages

--- bot.py
+++ bot.py
@@ -17,17 +17,27 @@ class TeleBot:
 
     def main_func(self):
         for m in self.bot.getUpdates(timeout=10):
+            if not m.message:
+                continue
+
             is_forwarded = m.message.forward_from
             forward_date = m.message.forward_date
             chat_id = m.message.chat_id
+
             if is_forwarded or forward_date:
+                if not m.message.forward_from_chat:
+                    continue
                 if m.message.forward_from_chat.id in get_banlist():
-                    self.bot.kickChatMember(chat_id, m.message.from_user.id)
-                    self.bot.sendMessage(chat_id=chat_id, text='%s отправлен в Вальхаллу [SPAM]' % m.message.from_user.name)
+                    try:
+                        self.bot.kickChatMember(chat_id, m.message.from_user.id)
+                        self.bot.sendMessage(chat_id=chat_id,
+                                             text='%s отправлен в Вальхаллу [SPAM]' % m.message.from_user.name)
+                    except Exception as e:
+                        print(e)
+                        self.bot.sendMessage(chat_id=chat_id,
+                                             text='%s спамит, но кикнуть не могу' % m.message.from_user.name)
 
-            update_id = m.update_id
-
-            self.confirm_updates(update_id)
+            self.confirm_updates(m.update_id)
 
     def confirm_updates(self, update_id):
         self.bot.getUpdates(offset=update_id+1, timeout=20)
